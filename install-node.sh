2c959c6536790ce73509eec5a8360e3725ab290e
gilleslabs <gilles.labs.tosi@gmail.com>
2017-01-23 20:33:15 +0100

    Added automatic registration of hosts in Default environment
    Signed-off-by: gilleslabs <gilles.labs.tosi@gmail.com>

--- install-node.sh
+++ install-node.sh
@@ -3,3 +3,6 @@ sudo echo nameserver 8.8.8.8 > /run/resolvconf/resolv.conf
 sudo ufw allow 500/udp
 sudo ufw allow 4500/udp
 sudo ufw allow 4789/udp
+
+##### Register host in Rancher in Default environment - To be studied how to use API - !!!!WARNING!!! Agent version may change => check in Rancher GUI
+sudo docker run -d --privileged -v /var/run/docker.sock:/var/run/docker.sock -v /var/lib/rancher:/var/lib/rancher rancher/agent:v1.1.3 http://192.168.99.100:8080
